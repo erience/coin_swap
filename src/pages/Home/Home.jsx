a6a2c6e5705339e19b2a93db1e450925d986940a
coinbase <erience>
2025-01-08 15:54:52 +0530

    change design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <section className="screen-wrapper">
       <div className="container">
-        <div className="relative w-full min-h-screen flex items-center justify-center">
-          <div className="relative w-full max-w-xl">
+        <div className="relative w-full min-h-screen flex items-end justify-center py-5">
+          <div className="relative w-full max-w-sm ml-auto">
             <SwapContainer />
           </div>
         </div>
