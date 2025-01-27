d8241f8bdb9a7be73883d4c07269490278435ac3
coinbase <erience>
2025-01-27 11:30:51 +0530

    new 2 cointer add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+import AquariumOfInterest from "@/components/AquariumOfInterest/AquariumOfInterest";
+import Finology from "@/components/Finology/Finology";
 import SwapContainer from "@/components/SwapContainer/SwapContainer";
 
 const Home = () => {
@@ -5,8 +7,19 @@ const Home = () => {
     <section className="screen-wrapper">
       <div className="container">
         <div className="relative w-full min-h-screen flex items-end justify-center py-5">
-          <div className="relative w-full max-w-sm ml-auto">
+          {/* <div className="relative w-full max-w-sm ml-auto">
             <SwapContainer />
+          </div> */}
+          <div className="grid grid-cols-12 gap-y-8 lg:gap-8">
+            <div className="col-span-12 lg:col-span-4">
+              <AquariumOfInterest />
+            </div>
+            <div className="col-span-12 lg:col-span-4">
+              <Finology />
+            </div>
+            <div className="col-span-12 lg:col-span-4">
+              <SwapContainer />
+            </div>
           </div>
         </div>
       </div>
